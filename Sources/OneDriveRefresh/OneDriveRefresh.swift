e9f01652aab7a0eaaafb96bf28b16cd7a4597c43
Underthestars-zhy <zhuhaoyu0909@icloud.com>
2022-04-27 13:41:29 +0800

    without async query

--- Sources/OneDriveRefresh/OneDriveRefresh.swift
+++ Sources/OneDriveRefresh/OneDriveRefresh.swift
@@ -34,6 +34,22 @@ public struct OneDriveRefresh {
     public func access() async throws -> String? {
         try await startPost()?.1
     }
+
+    public func accessWithAsync(_ handel: @escaping (String?, Error?) -> ()) throws -> Progress {
+        return try startPostWithoutAsync { res, error in
+            if let error = error {
+                handel(nil, error)
+                return
+            }
+
+            guard let res = res else {
+                handel(nil, nil)
+                return
+            }
+
+            handel(res.1, nil)
+        }
+    }
     
     func startPost() async throws -> (String?, String?)? {
         let header = ["Content-Type": "application/x-www-form-urlencoded"]
@@ -62,5 +78,53 @@ public struct OneDriveRefresh {
         let json = try JSON(data: data)
         
         return (json["refresh_token"]?.string, json["access_token"]?.string)
+    }
+
+    func startPostWithoutAsync(_ handel: @escaping ((String?, String?)?, Error?) -> ()) throws -> Progress {
+        let header = ["Content-Type": "application/x-www-form-urlencoded"]
+        var conponents = URLComponents()
+
+        conponents.queryItems = [
+            URLQueryItem(name: "client_id", value: clientId),
+            URLQueryItem(name: "refresh_token", value: recentToken),
+            URLQueryItem(name: "grant_type", value: "refresh_token"),
+        ]
+
+        guard let url = URL(string: "https://login.microsoftonline.com/common/oauth2/token") else { return Progress() }
+
+        var request = URLRequest(url: url)
+
+        request.httpMethod = "POST"
+        request.allHTTPHeaderFields = header
+        request.httpBody = conponents.query?.data(using: .utf8)
+
+        let session = URLSession.shared.dataTask(with: request) { data, response, error in
+            if let error = error {
+                handel(nil, error)
+                return
+            }
+
+            guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
+                handel(nil, nil)
+                return
+            }
+
+            guard let data = data else {
+                handel(nil, nil)
+                return
+            }
+
+            do {
+                let json = try JSON(data: data)
+                handel((json["refresh_token"]?.string, json["access_token"]?.string), nil)
+            } catch {
+                handel(nil, error)
+                return
+            }
+        }
+
+        session.resume()
+
+        return session.progress
     }
 }
